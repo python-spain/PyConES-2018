47ecc623cf500ae03555522c19d468bbb2c3ee9d
Adrián Matellanes <amatellanes@freepik.com>
2018-03-06 00:08:12 +0100

    Add form to change language

--- pyconesweb/urls.py
+++ pyconesweb/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('newsletter/', include('newsletter.urls')),
     path('', include('web.urls')),
 ]
