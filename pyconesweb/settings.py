0ed2b8ffceb3a24dff76fa5ef291e8dc03329961
Adrián Matellanes <amatellanes@freepik.com>
2018-04-06 09:04:41 +0200

    Fix redis location

--- pyconesweb/settings.py
+++ pyconesweb/settings.py
@@ -94,14 +94,14 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/0',
+        'LOCATION': 'redis://redis:6379/0',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
     },
     'compressor': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': 'redis://redis:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
