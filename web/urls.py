bdee15e6d467365efeb01fbfdf6d72d4556c488b
Marcos Gabarda <hey@marcosgabarda.com>
2018-06-19 13:54:43 +0200

    more simple urls

--- web/urls.py
+++ web/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from web.views import IndexView, DetailsView
 
 urlpatterns = [
-    path(r'details/<path:path>', DetailsView.as_view(), name="details"),
+    path(r'<path:path>', DetailsView.as_view(), name="details"),
     path('', IndexView.as_view())
 ]
