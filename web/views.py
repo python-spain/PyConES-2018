271ff33e58ec3b881d17bcf8c30c7a419ab96475
Marcos Gabarda <hey@marcosgabarda.com>
2018-07-14 00:42:05 +0200

    new urls schema

--- web/views.py
+++ web/views.py
@@ -1,8 +1,10 @@
 import os
+import re
 
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import TemplateDoesNotExist
+from django.urls import reverse
 from django.views.generic import TemplateView, View
 
 
@@ -14,8 +16,12 @@ class DetailsView(View):
     base_path = 'details'
 
     def get(self, request, path):
+        match = re.match(r".+/$", path)
+        if match:
+            path = re.sub(r"/$", "", path)
+            return redirect(reverse("details", kwargs={"path": path}), permanent=True)
         try:
-            template_path = os.path.join(self.base_path, path)
+            template_path = "{}.html".format(os.path.join(self.base_path, path))
             return render(request, template_path)
         except TemplateDoesNotExist:
             raise Http404()
