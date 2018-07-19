b1620a709aeb100ec5e96edb7344d1272c2b3d61
Marcos Gabarda <hey@marcosgabarda.com>
2018-07-19 17:10:09 +0200

    added --clear to collectstatic

--- run_web.sh
+++ run_web.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 python manage.py migrate
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 python manage.py compilemessages
 exec gunicorn --env DJANGO_SETTINGS_MODULE=pyconesweb.prod pyconesweb.wsgi -b 0.0.0.0:8000
