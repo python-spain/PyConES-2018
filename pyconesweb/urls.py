18a2e59325898b3525dee7a8294ffa67e921decb
Salvador Marí <saruba@gmail.com>
2018-09-13 13:29:36 +0200

    simple 404 page

--- pyconesweb/urls.py
+++ pyconesweb/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.defaults import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
     path('newsletter/', include('newsletter.urls')),
+    path('404/', page_not_found,  {'exception': Exception()}),
     path('', include('web.urls')),
 ]
