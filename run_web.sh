b6e6d59ac87fe571f96305241955b7a0564124fe
Marcos Gabarda <hey@marcosgabarda.com>
2018-11-21 12:35:02 +0100

    testing gunicorn config

--- run_web.sh
+++ run_web.sh
@@ -3,4 +3,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput --clear
 python manage.py compilemessages
-exec gunicorn --log-level=debug --timeout 10 --env DJANGO_SETTINGS_MODULE=pyconesweb.prod pyconesweb.wsgi -b 0.0.0.0:8000
+exec gunicorn --worker-class eventlet --log-level=debug --timeout 60 --env DJANGO_SETTINGS_MODULE=pyconesweb.prod pyconesweb.wsgi -b 0.0.0.0:8000
