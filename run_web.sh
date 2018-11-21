f7ade9b915c2ea5a3dfb121c8097228fdeddce6c
Marcos Gabarda <hey@marcosgabarda.com>
2018-11-21 13:13:29 +0100

    more workers

--- run_web.sh
+++ run_web.sh
@@ -3,4 +3,10 @@
 python manage.py migrate
 python manage.py collectstatic --noinput --clear
 python manage.py compilemessages
-exec gunicorn --worker-class eventlet --log-level=debug --timeout 60 --env DJANGO_SETTINGS_MODULE=pyconesweb.prod pyconesweb.wsgi -b 0.0.0.0:8000
+exec gunicorn pyconesweb.wsgi \
+    --worker-class eventlet \
+    --workers 3 \
+    --log-level=debug \
+    --timeout 60 \
+    --env DJANGO_SETTINGS_MODULE=pyconesweb.prod \
+    -b 0.0.0.0:8000
